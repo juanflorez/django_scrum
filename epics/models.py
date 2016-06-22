19fd179449abaa6513dbdb0c4794ed5902f9fd22
juan <florez.esteban@gmail.com>
2016-06-22 11:11:16 +0300

    Introduced ABSOLUTE_URLS in the model for epic, and used it in the lists html template to improve the dynamic links

--- epics/models.py
+++ epics/models.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.core.urlresolvers import reverse
 
 # Create your models here.
 
@@ -8,7 +9,15 @@ class Epic(models.Model):
     title   =  models.CharField(max_length = 120)
     content =  models.TextField()
     points  =  models.IntegerField()
-    
 
-    def __str__(self):
-    	return self.title
+
+    def __str__(self):  
+	    return self.title
+
+    def __unicode__(self): 
+	    return self.title
+
+
+    def get_absolute_url(self):
+    	return reverse("detail", kwargs={"id": self.id})
+        
